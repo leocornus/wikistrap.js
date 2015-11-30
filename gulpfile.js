c5a28c781376f1da9b7b2cb3cf7c21a52227317a
Sean Chen <sean.chen@leocorn.com>
2015-11-30 12:22:46 -0500

    using express and gulp-live-server to simplefy e2e testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 
 // set the default task.
-gulp.task('default', ['karma', 'protractor', 'exit']);
+gulp.task('default', ['unit-test', 'e2e-test']);
+
+// the unit test tasks.
+gulp.task('unit-test', ['karma']);
+// the e2e test tasks.
+gulp.task('e2e-test', ['protractor', 'express-app-stop']);
 
 // a quick test task.
 gulp.task('hello', function() {
@@ -31,6 +36,23 @@ gulp.task('webserver', function() {
     })).on('error', function(e) {throw e;});
 });
 
+// using the express to serve static files.
+var gls = require('gulp-live-server');
+// the simplest express static server.
+var liveServer = gls.static('.', 8900);
+// using a simple javascript file for express server.
+//var liveServer = gls.new('test/express/simple.js');
+gulp.task('express-app', function() {
+
+    liveServer.start();
+});
+
+// stop live server after e2e testing are finished.
+gulp.task('express-app-stop', ['protractor'], function() {
+
+    liveServer.stop();
+});
+
 // get ready for protractor.
 var webdriver_update = require('gulp-protractor').webdriver_update;
 var webdriver = require('gulp-protractor').webdriver;
@@ -42,12 +64,17 @@ gulp.task('webdriver', webdriver);
 var protractor = require('gulp-protractor').protractor;
 // protractor e2e test will depend on webserver and 
 // webdriver task.
-gulp.task('protractor', ['webserver', 'webdriver_update', 'webdriver'], function() {
+gulp.task('protractor', 
+          ['express-app', 'webdriver_update', 'webdriver'], 
+          function() {
 
     return gulp.src(['test/e2e/*.js']).pipe(protractor({
         configFile: 'test/protractor.conf.js'
-    })).on('error', function(e) {throw e});
-
+    })).on('error', function(e) {
+        console.log(e.toString());
+        // proper way to stop the task when error happen.
+        this.emit("end");
+    });
 });
 
 // the exit task will stop web server at the end of testing.
