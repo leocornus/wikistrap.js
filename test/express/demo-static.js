8c1b40f272d1570253b43d2ed8820756ac3ab873
Sean Chen <sean.chen@leocorn.com>
2015-12-04 14:15:37 -0500

    using serve-index for express to show directory indexing

--- test/express/demo-static.js
+++ test/express/demo-static.js
@@ -1,12 +1,17 @@
 // the very simple express static server.
 
 var express = require('express');
+// serve-index provide directory indexing.
+var serveIndex = require('serve-index');
 var app = express();
 
 //load static files.
 app.use('/demo', express.static('demo'));
+app.use('/demo', serveIndex('demo'));
 app.use('/src', express.static('src'));
+app.use('/src', serveIndex('src'));
 app.use('/bower_components', express.static('bower_components'));
+app.use('/bower_components', serveIndex('bower_components'));
 
 // hello world simple get.
 app.get('/hello', function(req, res) {
