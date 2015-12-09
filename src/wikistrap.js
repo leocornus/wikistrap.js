dbc1226ad61deb8d1c3f52b5ad00ffab73bb6bea
Sean Chen <sean.chen@leocorn.com>
2015-12-09 14:38:32 -0500

    create the function to get any wiki article from a wiki site.

--- src/wikistrap.js
+++ src/wikistrap.js
@@ -58,6 +58,9 @@
                 );
             }
 
+            /**
+             * direct HTTP get method.
+             */
             jQuery.get(self.getApiUrl(), action).
                 done(function(data) {
                     // self.log(data);
@@ -78,6 +81,157 @@
                       this.siteOptions.baseUrl + 
                       this.siteOptions.apiPath;
             return url;
+        },
+
+        /**
+         * get a wiki article.
+         *
+         * @param {string} title the page title, which should 
+         *        be unique in a wiki site.
+         */
+        getArticle: function(title, callback) {
+
+            // save the original this.
+            var self = this;
+
+            // get ready the data send to wiki api.php.
+            var action = {
+                'format' : 'json',
+                'action' : 'parse',
+                'page' : title,
+                'prop' : 'text'
+            };
+
+            // call wiki api
+            self.apiGet(action, function(err, data) {
+
+                if(err) {
+                    // got error.
+                    callback(err);
+                    return;
+                }
+
+                // get the page content html from response.
+                var content = data.parse.text['*'];
+                // process the content to match bootstrap.
+                content = self.createArticleRow(content);
+                callback(null, content);
+            });
+        },
+
+        /**
+         * create bootstrap row for wiki article content.
+         */
+        createArticleRow: function(content) {
+
+            var self = this;
+
+            var processed = this.processArticleContent(content);
+            var contentHtml = processed['content'];
+            var tocHtml = processed['toc'];
+            var rowHtml = '<div class="row">' +
+                '  <div class="col-md-9" id="content">' + 
+                contentHtml + 
+                '  </div>' + 
+                '  <div class="col-md-3" id="navcol">' + 
+                tocHtml +
+                '  </div>' + 
+                '</div>';
+
+            // hook the resize event.
+            jQuery(window).on('resize', function() {
+
+                self.syncSidenavWidth();
+            });
+
+            var $row = jQuery(rowHtml);
+            return $row;
+        },
+
+        /**
+         * utility method to process the wiki article HTML
+         */
+        processArticleContent: function(content) {
+
+            // parse the content html to a jQuery object.
+            var $content = jQuery('<div>').html(content);
+            // find the TOC div.
+            var $toc = $content.find('div#toc>ul');
+            //$toc.find('a').attr('data-toggle', 'pill');
+            var liHtml = $toc.html();
+
+            var navPanel = '<div class="panel panel-info' +
+                '                sidebar-nav-fixed affix-top"' +
+                '                id="navpanel"' + 
+                '                style="margin-left: -15px">' + 
+                '  <div class="panel-heading">' +
+                '    <strong>Contents</strong>' + 
+                '  </div>' +
+                '  <div id="sidenav">' + 
+                '    <ul class="nav nav-pills nav-stacked"' +
+                '        style="max-height: 450px; ' + 
+                '               overflow-y: auto">' +
+                liHtml + 
+                '    </ul>' +
+                '  </div>' + 
+                '</div>';
+
+            //$toc.find('div#toctitle').replaceWith('');
+            //alert(tocHtml);
+            // convert the toc to bootstrap scroll spy.
+            var $nav = jQuery('<nav class="affix" id="sidenav">').
+                       html($toc.html());
+            // add 
+            $nav.find('ul').addClass('nav');
+            // add class for the first ul.
+            $nav.children('ul').addClass('nav-pills nav-stacked')
+                .attr('data-spy', 'affix').attr('id', 'thenav');
+            //$nav.find('a').attr('data-toggle', 'pill');
+            // remove all class for li
+            $nav.find('li').attr('class', '');
+            var $toc = jQuery('<div>').html($nav);
+
+            // adding the scroll styp for body:
+            //var $body = jQuery('body');
+            //$body.attr('data-spy', 'scroll').attr('data-target', '#sidenav');
+
+            // remove TOC from content.
+            $content.find('div#toc').replaceWith('');
+            // remove the edit seciton for each heading.
+            $content.find('span.mw-editsection').replaceWith('');
+            var contentHtml = $content.html();
+            // replace globaly! all occurrence!
+            contentHtml = contentHtml.replace(/"\/wiki\//g, 
+               '"//' + this.siteOptions.baseUrl + '/wiki/');
+
+            var ret = {'toc' : navPanel, 
+                       'content' : contentHtml};
+            return ret; 
+        },
+
+        /**
+         * utility method to set the outer width of target object to 
+         * be the same with the source object.
+         */
+        syncSidenavWidth: function() {
+
+            var $source = jQuery('#navcol');
+            var $target = jQuery('#navpanel');
+            $target.outerWidth($source.outerWidth());
+            this.setAffixSpy();
+        },
+
+        /**
+         * set the affix spy for nave panel.
+         */
+        setAffixSpy: function() {
+            var $panel = jQuery('#navpanel');
+            $panel.affix({
+                offset: {
+                    // in pixel.
+                    top: this.siteOptions.affixOffsetTop
+                }
+            });
         },
     });
 
