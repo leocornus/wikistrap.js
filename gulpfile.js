91372c424823b6916a83f9094dda082e7061cfb1
Sean Chen <sean.chen@leocorn.com>
2015-12-08 19:19:50 -0500

    adding the dependence on nodemw for development testing

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ var gls = require('gulp-live-server');
 // the simplest express static server.
 //var liveServer = gls.static('.', 8900);
 // using a simple javascript file for express server.
-var liveServer = gls.new('test/express/demo-static.js');
+var liveServer = gls.new('test/express/local-app.js');
 gulp.task('express-app', function() {
 
     liveServer.start();
