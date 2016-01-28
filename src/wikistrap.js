4a9d36a7d43ab5d6b3221cef23e0896e8f0b8963
Sean Chen <sean.chen@leocorn.com>
2016-01-28 09:13:57 -0500

    code format update.

--- src/wikistrap.js
+++ src/wikistrap.js
@@ -662,11 +662,13 @@
             var rows = [];
             jQuery.each(images, function(pageId, page) {
 
+                // get the image url.
+                var imageUrl = page['imageinfo'][0]['url'];
                 var aRow = '<div class="row">' +
                     '  <div class="col-md-6">' +
                     '    <div class="thumbnail">' +
                     '    <img style="height: 500px"' + 
-                    '         src="' + page['imageinfo'][0]['url'] + '"' +
+                    '         src="' + imageUrl + '"' +
                     '         id="pageid-' + page['pageid'] + '"' +
                     '         pageTitle="' + page['title'] + '"' +
                     '    >' +
@@ -674,7 +676,8 @@
                     '  </div>' +
                     '  <div class="col-md-6"' +
                     '       id="image-desc-' + page['pageid'] + '"' +
-                    '       style="max-height: 500px; overflow-y: auto"' +
+                    '       style="max-height: 500px; ' +
+                    '              overflow-y: auto"' +
                     '  >' +
                     '  </div>' +
                     '</div>';
