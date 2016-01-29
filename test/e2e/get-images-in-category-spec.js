89e2937e88b27fe2b53578841f0031230fbc0e26
Sean Chen <sean.chen@leocorn.com>
2016-01-29 09:30:54 -0500

    wait until element present is better than sleep

--- test/e2e/get-images-in-category-spec.js
+++ test/e2e/get-images-in-category-spec.js
@@ -11,6 +11,8 @@ describe('Basic specs for get images in category', function() {
 
         loadCategory = element(by.id('load-cat'));
         category = element(by.id('cat-title'));
+        // we need the content container to verify some results.
+        content = element(by.id('content-container'));
     });
 
     it('should load the get images in category demo page', 
@@ -42,12 +44,25 @@ describe('Basic specs for get images in category', function() {
         loadCategory.click();
 
         // sleep for response comming.
-        browser.sleep(3000);
+        //browser.sleep(3000);
+        // wait until the div with class .row present.
+        browser.wait(function() {
+            return content.isElementPresent(by.css('.row'));
+        }
+        // we could set wait timeout here in ms.
+        //, 3000
+        // and set timeout message here:
+        //, 'it should present in 3 seconds'
+        );
 
         var row = element(by.css('.row'));
         //console.log(row.getTagName());
         expect(row.getTagName()).toBe('div');
         //expect(row).not.toBe(0);
         //expect(row).toEqual(20);
+
+        // check all rows.
+        var rows = element.all(by.css('.row'));
+        expect(rows.count()).toBe(3);
     });
 });
