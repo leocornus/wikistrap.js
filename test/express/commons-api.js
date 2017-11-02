399c6d2ac08684065ded271d689231971dca888f
Sean Chen <sean.chen@leocorn.com>
2017-11-02 12:55:47 -0400

    play around the search and replace.

--- test/express/commons-api.js
+++ test/express/commons-api.js
@@ -3,7 +3,8 @@ var bot = require('nodemw');
 var client = new bot({
     server: 'commons.wikimedia.org',
     path: '/w',
-    debug: false 
+    //path: '/wiki',
+    debug: true 
 });
 
 var api = function(req, res) {
@@ -11,8 +12,12 @@ var api = function(req, res) {
     //console.log(req.query);
 
     client.api.call(req.query, function(err, info, next, data) {
-        //console.log(data);
-        res.send(data);
+        console.log(data);
+        var baseUrlPattern = new RegExp("https://upload.wikimedia.org/wikipedia/commons", "g");
+        var processed = JSON.stringify(data).replace(baseUrlPattern, "/images");
+        console.log("=========================processed=========");
+        console.log(processed);
+        res.send(JSON.parse(processed));
     });
 };
 
