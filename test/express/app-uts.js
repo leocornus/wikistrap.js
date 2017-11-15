6b2e297087c2114b26baea1e579d1604b786c4cd
Sean Chen <sean.chen@leocorn.com>
2017-11-14 19:15:48 -0500

    add one more app for demo

---
+++ test/express/app-uts.js
@@ -0,0 +1,43 @@
+/**
+ * local express app server for tesint purpose.
+ */
+
+var express = require('express');
+// serve-index provide directory indexing.
+// this is for development purpose.
+var serveIndex = require('serve-index');
+// local settings.
+var settings = require('./local/uts.js');
+
+var app = express();
+
+//load static files web resources.
+app.use('/demo', express.static('demo'));
+app.use('/demo', serveIndex('demo'));
+app.use('/src', express.static('src'));
+app.use('/src', serveIndex('src'));
+app.use('/bower_components', express.static('bower_components'));
+app.use('/bower_components', serveIndex('bower_components'));
+
+// set the favicon.ico.
+app.get('/favicon.ico', function(req, res) {
+    res.sendFile('uts.ico', {root: __dirname});
+});
+
+// set the homepage.
+app.get('/', function(req, res) {
+    res.sendFile('uts.html', {root: __dirname + '/../../demo'});
+});
+
+// direct images
+var imagesApi = require('./images-api.js');
+// set options.
+imagesApi.setOptions({
+    "remoteUrl": settings.config.remoteUrl,
+    "localUrl" : settings.config.localUrl
+});
+app.get(/^\/images/, imagesApi.api);
+
+// start the express server.
+var server = app.listen(settings.config.expressPort, function() {
+});
